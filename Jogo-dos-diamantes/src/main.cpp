154b4fc5540e00d8a604e8761c1e3d1571fd7c0d
Lucas Augusto <nutkaugusto@gmail.com>
2020-10-19 15:49:13 -0300

    passed on tests

--- Jogo-dos-diamantes/src/main.cpp
+++ Jogo-dos-diamantes/src/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <vector>
 
-int main(){
+int max(int a, int b){
+	if(a>b)
+		return a;
+	return b;
+}
 
-	// Início entrada
+int sumOfVectorElements(std::vector<int> &arr){
+	int sum = 0;
+	for(auto it=arr.begin(); it!=arr.end(); ++it)
+		sum += *it;
+	return sum;
+}
+
+int solution(std::vector<int> &weights){
+	int sumOfWeights = sumOfVectorElements(weights);
+	const int row = weights.size() + 1;
+	const int col = sumOfWeights/2 + 1;
+	std::vector<std::vector<int>> matrix(row, std::vector<int>(col, 0));
+	
+	for(int i=1; i<row; i++){
+		for(int j=1; j<col; j++){
+			if(weights.at(i-1)<=j){
+				int param = weights.at(i-1) + matrix[i-1][j-weights.at(i-1)];
+				matrix[i][j] = max(matrix[i-1][j], param);
+			}else
+				matrix[i][j] = matrix[i-1][j];
+		}
+	}
+
+	return sumOfWeights-(2*matrix[row-1][col-1]);
+}
+
+int main(){
 	int amountsOfDiamonds;
 	std::vector<int> weights;
 
@@ -15,5 +45,6 @@ int main(){
 		weights.push_back(weight);
 	}
 
+	std::cout << solution(weights) << std::endl;
 	return 0;
 }
